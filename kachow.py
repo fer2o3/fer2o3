8e5cfb5ea5d1826daffa0d622cd57fff3f1b51fd
Benjamin Andre <benjaminandre23@gmail.com>
2025-08-14 08:58:58 +0200

    feat(svg): separated loc into add and rem

--- kachow.py
+++ kachow.py
@@ -94,6 +94,105 @@ def update_dots(root, total_width=47):
             tspan.text = " " + "." * dots_needed + " "
 
 
+def update_lines(root, username="fer2o3", total_width=47):
+    token = os.getenv("README_TOKEN")
+    headers = {"Authorization": f"token {token}"}
+
+    query = """
+    query($username: String!) {
+        user(login: $username) {
+            repositories(first: 50, ownerAffiliations: OWNER) {
+                nodes {
+                    name
+                    owner {
+                        login
+                    }
+                }
+            }
+        }
+    }
+    """
+
+    response = requests.post(
+        "https://api.github.com/graphql",
+        json={"query": query, "variables": {"username": username}},
+        headers=headers,
+    )
+
+    if response.status_code != 200:
+        return
+
+    repos = response.json()["data"]["user"]["repositories"]["nodes"]
+    lines_added = 0
+    lines_removed = 0
+
+    for repo in repos:
+        repo_query = """
+        query($name: String!, $owner: String!) {
+            repository(name: $name, owner: $owner) {
+                defaultBranchRef {
+                    target {
+                        ... on Commit {
+                            history(first: 100) {
+                                edges {
+                                    node {
+                                        author {
+                                            user {
+                                                login
+                                            }
+                                        }
+                                        additions
+                                        deletions
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        """
+
+        repo_response = requests.post(
+            "https://api.github.com/graphql",
+            json={
+                "query": repo_query,
+                "variables": {"name": repo["name"], "owner": repo["owner"]["login"]},
+            },
+            headers=headers,
+        )
+
+        if repo_response.status_code == 200:
+            data = repo_response.json()
+            if data["data"]["repository"]["defaultBranchRef"]:
+                commits = data["data"]["repository"]["defaultBranchRef"]["target"][
+                    "history"
+                ]["edges"]
+                for commit in commits:
+                    if (
+                        commit["node"]["author"]["user"]
+                        and commit["node"]["author"]["user"]["login"] == username
+                    ):
+                        lines_added += commit["node"]["additions"]
+                        lines_removed += commit["node"]["deletions"]
+
+    add_text = f"{lines_added} ++"
+    rem_text = f" {lines_removed} --"
+
+    update_svg_element(root, "add", add_text)
+    update_svg_element(root, "rem", rem_text)
+
+    add = root.find(".//*[@id='add']")
+    rem = root.find(".//*[@id='rem']")
+    dots = add.getprevious()
+
+    add_length = len(add.text) if add is not None else 0
+    rem_length = len(rem.text) if rem is not None else 0
+    used_width = len(" lines of code") + add_length + rem_length + 2
+    dots_needed = max(1, total_width - used_width)
+    dots.text = " " + "." * dots_needed + " "
+
+
 def update_svg_element(root, id, value):
     element = root.find(f".//*[@id='{id}']")
     if element is not None:
@@ -117,8 +216,8 @@ def update_svg_file(filepath):
     update_svg_element(root, "stars", stats["stars"])
     update_svg_element(root, "commits", stats["commits"])
     update_svg_element(root, "contributions", stats["contributions"])
-    update_svg_element(root, "lines", stats["lines"])
     update_dots(root)
+    update_lines(root)
 
     with open(filepath, "w") as f:
         f.write(etree.tostring(root, encoding="unicode", pretty_print=True))
